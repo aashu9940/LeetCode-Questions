f301cda0325f14a1bf7f8f0c78e72b72c374dbf7
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-05-19 16:23:58 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (25.31%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        
+        
+        
+        
+        
+        
+    }
+};
