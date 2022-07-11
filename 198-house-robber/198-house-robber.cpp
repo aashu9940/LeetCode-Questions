33dafbe5398415c724b7873f3713b1d2dcefbed9
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-07-11 14:37:21 +0530

    Time: 3 ms (54.03%), Space: 7.6 MB (75.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
-        int neg = 0;
+        
+        int prev =nums[0];
+        int prev2 = 0;
         
         for(int i=1;i<n;i++){
             int take = nums[i];
-            if(i>1) take+=dp[i-2];
-            int not_take = 0+dp[i-1];
-            dp[i] = max(take,not_take);
+            if(i>1) take+=prev2;
+            int not_take = 0+prev;
+            int curi = max(take,not_take);
+            prev2 = prev;
+            prev = curi;
             
         }
-        return dp[n-1];
+        return prev;
     }
 };
 
@@ -40,5 +42,28 @@ public:
      
 //         return f(n-1,nums,dp);
         
+//     }
+// };
+
+
+
+// TABULATION :-
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>dp(n,0);
+//         dp[0]=nums[0];
+//         int neg = 0;
+        
+//         for(int i=1;i<n;i++){
+//             int take = nums[i];
+//             if(i>1) take+=dp[i-2];
+//             int not_take = 0+dp[i-1];
+//             dp[i] = max(take,not_take);
+            
+//         }
+//         return dp[n-1];
 //     }
 // };
