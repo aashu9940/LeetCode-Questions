6b93c4067bb199a042e6d913606f8f9f1f979a5a
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-07-11 14:34:48 +0530

    Time: 3 ms (54.03%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,44 @@
 class Solution {
-    int f(int ind,vector<int>& nums,vector<int>&dp){
-        if(ind==0)return nums[ind];
-        if(ind<0)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        
-        int pick = nums[ind]+f(ind-2,nums,dp);
-        int not_pick = 0+f(ind-1,nums,dp);
-        
-        return dp[ind] =  max(pick,not_pick);
-    }
 public:
     int rob(vector<int>& nums) {
-           int n = nums.size();
-        vector<int>dp(n,-1);
-     
-        return f(n-1,nums,dp);
+        int n = nums.size();
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        int neg = 0;
         
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int not_take = 0+dp[i-1];
+            dp[i] = max(take,not_take);
+            
+        }
+        return dp[n-1];
     }
 };
+
+
+
+
+//     MEMOIZATION :-
+
+// class Solution {
+//     int f(int ind,vector<int>& nums,vector<int>&dp){
+//         if(ind==0)return nums[ind];
+//         if(ind<0)return 0;
+//         if(dp[ind]!=-1)return dp[ind];
+        
+//         int pick = nums[ind]+f(ind-2,nums,dp);
+//         int not_pick = 0+f(ind-1,nums,dp);
+        
+//         return dp[ind] =  max(pick,not_pick);
+//     }
+// public:
+//     int rob(vector<int>& nums) {
+//            int n = nums.size();
+//         vector<int>dp(n,-1);
+     
+//         return f(n-1,nums,dp);
+        
+//     }
+// };
