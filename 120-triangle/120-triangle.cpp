ae2e50fe573018ced9bb14712a87d3343691e740
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-07-24 14:47:56 +0530

    Time: 6 ms (83.65%), Space: 8.8 MB (48.98%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,40 @@
 class Solution {
-    int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp,int n){
-        if(i==n-1)return triangle[n-1][j];
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int down = triangle[i][j]+f(i+1,j,triangle,dp,n);
-        int diagonal = triangle[i][j]+f(i+1,j+1,triangle,dp,n);
-        
-        return dp[i][j] = min(down,diagonal);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int dp[n][n];
         
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
         
-        return f(0,0,triangle,dp,n);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j]+dp[i+1][j];
+                int diagonal = triangle[i][j]+dp[i+1][j+1];
+                dp[i][j] = min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
+//MEMOIZATION
+
+// int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp,int n){
+//         if(i==n-1)return triangle[n-1][j];
+//         if(dp[i][j]!=-1)return dp[i][j];
+        
+//         int down = triangle[i][j]+f(i+1,j,triangle,dp,n);
+//         int diagonal = triangle[i][j]+f(i+1,j+1,triangle,dp,n);
+        
+//         return dp[i][j] = min(down,diagonal);
+//     }
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+        
+//         int n = triangle.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+        
+//         return f(0,0,triangle,dp,n);
+//     }
