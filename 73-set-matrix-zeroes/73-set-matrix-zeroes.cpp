a3deaf0d971a7a209dac60bf03b6078468f752f0
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-08-05 14:28:05 +0530

    Time: 16 ms (84.59%), Space: 13.4 MB (18.37%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,24 +3,26 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-        vector<pair<int,int>>v;
         
+        vector<int>dummy1(r,-1);
+        vector<int>dummy2(c,-1);
         
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(matrix[i][j]==0){
-                    v.push_back(make_pair(i,j));
+                    dummy1[i]=0;
+                    dummy2[j]=0;
                 }
             }
         }
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<c;j++)
-                matrix[v[i].first][j]=0;
-            for(int j=0;j<r;j++)
-                matrix[j][v[i].second]=0;
-                
-            
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(dummy1[i]==0 or dummy2[j]==0){
+                    matrix[i][j]=0;
+                }
+            }
         }
+        
        
     }
 };
