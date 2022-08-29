55dd8c7611c22daeb681066e0c985c934facaf7c
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-08-29 14:28:51 +0530

    Time: 31 ms (34.47%), Space: 9 MB (59.00%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,19 +1,22 @@
 class Solution {
-    
 public:
-    int f(int i,int j,string &str1,string &str2,vector<vector<int>>&dp){
-        if(i==0)return j;
-        if(j==0)return i;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(str1[i-1]==str2[j-1])return dp[i][j]=0+f(i-1,j-1,str1,str2,dp);
-        return dp[i][j] = 1+min(f(i,j-1,str1,str2,dp),min(f(i-1,j,str1,str2,dp),f(i-1,j-1,str1,str2,dp)));
-    }
-    
     int minDistance(string str1, string str2) {
+        
         int n = str1.size();
         int m = str2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=0;i<=n;i++)dp[i][0]=i;
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,str1,str2,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]) dp[i][j]=dp[i-1][j-1];
+                
+                else dp[i][j] = 1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        
+        
+        return dp[n][m];
     }
 };
