783c1a0044b574524eacfb28220de9cc8cf3e533
Ashutosh Gupta <73346860+aashu9940@users.noreply.github.com>
2022-08-29 14:21:50 +0530

    Time: 12 ms (82.21%), Space: 9.2 MB (23.39%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,10 +2,10 @@ class Solution {
     
 public:
     int f(int i,int j,string &str1,string &str2,vector<vector<int>>&dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+        if(i==0)return j;
+        if(j==0)return i;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(str1[i]==str2[j])return dp[i][j]=0+f(i-1,j-1,str1,str2,dp);
+        if(str1[i-1]==str2[j-1])return dp[i][j]=0+f(i-1,j-1,str1,str2,dp);
         return dp[i][j] = 1+min(f(i,j-1,str1,str2,dp),min(f(i-1,j,str1,str2,dp),f(i-1,j-1,str1,str2,dp)));
     }
     
@@ -13,7 +13,7 @@ public:
         int n = str1.size();
         int m = str2.size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,str1,str2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,str1,str2,dp);
     }
 };
